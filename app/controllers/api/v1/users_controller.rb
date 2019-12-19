50c06ca2cd5f3ba6826c7bc725d28b1977eed916
Oliver Senduk <oliver@ichistudios.com>
2019-12-19 17:23:17 +0100

    FEAT || USER CREATE adds tests & code for creating users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,21 @@
 class Api::V1::UsersController < ApplicationController
     def show
-        render json: User.find(params[:id]), status: 200
+        render json: User.find(params[:id]), status: :ok
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user.errors.messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -5,18 +5,44 @@ describe Api::V1::UsersController, type: :controller do
 
     context 'CRUD actions' do
         context 'SHOW - should return a user' do
-
             before do 
                 get api_v1_user_url(user.id), as: :json
             end
 
             it 'returns http 200 success' do
-                expect(response.status).to eq 200
+                expect(last_response.status).to eq 200
             end
 
             it 'should contain the correct information' do
                 user_json = parse_response_json
                 expect(user.email).to eq(user_json['email'])
+            end
+        end
+
+        context 'CREATE - should create a new user' do
+            it 'creates a new user' do 
+                post api_v1_users_url, 
+                {
+                    user: {
+                        email: 'new-user@test.com',
+                        password: '12345678',
+                    }
+                }, as: :json
+                expect(last_response.status).to eq 201
+                expect(User.last.email).to eq('new-user@test.com')
+            end
+
+            it 'will not create a user if the email is taken' do
+                post api_v1_users_url, 
+                    {
+                        user: {
+                            email: user.email,
+                            password: '12345678',
+                            something: 'testing'
+                        }
+                    }, as: :json
+
+                expect(last_response.status).to eq 422
             end
         end
     end
